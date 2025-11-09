f912c99f287f4530ceb0a96f85df2ee7b9e42fae
PavaniReddy H M <pavanireddy082003@gmail.com>
2025-11-09 06:28:52 +0530

    Time: 1 ms (99.89%), Space: 46.9 MB (8.12%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,3 @@
-/**
-Hi Pavani
-this code applies only when numbers repeat twice and thrice 
-since XOR cancels out if two numbera are same X^X=0
- */
 class Solution {
     public int singleNumber(int[] nums) {
         int res=0;
