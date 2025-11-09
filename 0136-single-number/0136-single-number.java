3ae291e1772fda3d0a95470b8cfde964c3635144
PavaniReddy H M <pavanireddy082003@gmail.com>
2025-11-09 06:30:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,3 +1,8 @@
+/**
+Hi Pavani
+this code applies only when numbers repeat twice and thrice 
+since XOR cancels out if two numbera are same X^X=0
+ 
 class Solution {
     public int singleNumber(int[] nums) {
         int res=0;
@@ -8,3 +13,32 @@ class Solution {
         
     }
 }
+
+BRUTEFORCE APPROACH
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean isUnique = true; // assume nums[i] is unique
+
+            for (int j = 0; j < nums.length; j++) { // j starts from 0 (correct)
+                if (i != j && nums[i] == nums[j]) { // skip same index
+                    isUnique = false;
+                    break; // no need to check further
+                }
+            }
+
+            // If array has only one element, return it directly
+            if (nums.length == 1) {
+                return nums[i];
+            }
+            // If current number is unique, return it
+            else if (isUnique) {
+                return nums[i];
+            }
+        }
+
+        // if nothing found (shouldn't happen for valid input)
+        return -1;
+    }
+*/
+    
