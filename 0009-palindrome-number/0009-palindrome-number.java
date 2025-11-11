bd5eb4ede9abdf415d1c09eb7499ac562c868ac2
PavaniReddy H M <pavanireddy082003@gmail.com>
2025-11-11 11:43:13 +0530

    Time: 5 ms (66.17%), Space: 45.9 MB (5.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -11,3 +11,28 @@ class Solution {
         return original==reversed;
     }
 }
+/**
+
+
+So you want to treat negative numbers as positive — meaning if the number is -121, we check it like 121 (✅ palindrome).
+than it is
+    n = Math.abs(n); // \U0001f448 convert to positive number
+
+
+class Solution {
+    boolean isPalindrome(int n) {
+        n = Math.abs(n); // \U0001f448 convert to positive number
+
+        int original = n;
+        int reversed = 0;
+
+        while (n > 0) {
+            int digit = n % 10;             // get last digit
+            reversed = reversed * 10 + digit; // build reversed number
+            n = n / 10;                     // remove last digit
+        }
+
+        return original == reversed;
+    }
+}
+ */
