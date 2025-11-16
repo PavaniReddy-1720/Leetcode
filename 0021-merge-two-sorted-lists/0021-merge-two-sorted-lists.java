978e993d44d4b807fcc87b5e36266a470bc1d262
PavaniReddy H M <pavanireddy082003@gmail.com>
2025-11-16 06:01:02 +0530

    Time: 0 ms (100%), Space: 44.4 MB (6.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,28 +10,61 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode returnNode=new ListNode(Integer.MIN_VALUE);
+        ListNode returnNode = new ListNode(Integer.MIN_VALUE);  // dummy node
+        ListNode headNode = returnNode;  // store head
 
-        ListNode headNode=returnNode;
+        while (l1 != null && l2 != null) {
 
-        while(l1!=null && l2!=null){
-            
-            if(l1.val<=l2.val){
-                returnNode.next=l1;
-                l1=l1.next;
+            if (l1.val <= l2.val) {
+                returnNode.next = l1;
+                l1 = l1.next;
+            } else {
+                returnNode.next = l2;
+                l2 = l2.next;
             }
-            else{
-                returnNode.next=l2;
-                l2=l2.next;
-            }
-            returnNode=returnNode.next;
+
+            returnNode = returnNode.next;
         }
 
-        if(l1==null){
-            returnNode.next=l2;
-        }else if(l2==null){
-            returnNode.next=l1;
+        // Add remaining nodes
+        if (l1 == null) {
+            returnNode.next = l2;
+        } else {
+            returnNode.next = l1;
         }
-    return returnNode;
+
+        // return the actual merged head
+        return headNode.next;
     }
 }
+
+/**
+BRUTEFORCE using array
+takes O(n) space Complexity
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    ArrayList<Integer> arr = new ArrayList<>();
+
+    while (l1 != null) {
+        arr.add(l1.val);
+        l1 = l1.next;
+    }
+
+    while (l2 != null) {
+        arr.add(l2.val);
+        l2 = l2.next;
+    }
+
+    Collections.sort(arr);
+
+    ListNode dummy = new ListNode(0);
+    ListNode curr = dummy;
+
+    for (int val : arr) {
+        curr.next = new ListNode(val);
+        curr = curr.next;
+    }
+
+    return dummy.next;
+}
+ */
