cda748e6032db8151bbbfa5dbb694653cf4fc18a
PavaniReddy H M <pavanireddy082003@gmail.com>
2025-11-17 06:25:06 +0530

    Create Multiply.java

---
+++ Multiply Two LinkedList/Multiply.java
@@ -0,0 +1,33 @@
+/*Node is defined as
+class Node
+{
+    int data;
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class Solution {
+    public long multiplyTwoLists(Node l1, Node l2) {
+        
+        long num1=0,num2=0;
+        
+        //to prevent Overflow
+        //for handling large numbers
+        long mod=1000000007;
+        //convert first LL to number
+        while(l1!=null){
+            num1=(num1*10 +l1.data)%mod;
+            l1=l1.next;
+        }
+        
+        //convert Second LL to number
+        while(l2!=null){
+            num2=(num2*10+l2.data)%mod;
+            l2=l2.next;
+        }
+        return (num1*num2)%mod;
+    }
+}
