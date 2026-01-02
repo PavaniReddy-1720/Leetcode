83efea3fc2eafcb5d734c4136e54d4e1de4db5c4
PavaniReddy H M <pavanireddy082003@gmail.com>
2026-01-02 20:56:36 +0530

    Time: 1 ms (89.36%), Space: 43.5 MB (71.91%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -4,5 +4,11 @@ class Solution {
         if(!(str1+str2).equals(str2+str1)){
             return "";
         }
+        int gcdLength=gcd(str1.length(),str2.length());
+        return str1.substring(0,gcdLength);
+    }
+    private int gcd(int a,int b){
+        if (b==0) return a;
+        return gcd(b,a%b);
     }
 }
